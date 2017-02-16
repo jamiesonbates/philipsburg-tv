aaee0e34a067d473389ec6e3de833248c3f192f7
Jamieson Bates <jamiesonbates@gmail.com>
2017-02-16 14:33:30 -0800

    Add page header/title with background graphic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Results from './components/Results';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Results />
       </div>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import '../css/Header.css';
+
+const Header = () =>
+  <div className="Header-container">
+      <h1>Philipsburg, Montana</h1>
+  </div>
+
+export default Header;
