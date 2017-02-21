28846747066b3b0c54f0d9b5e369bbc60c4c4c0c
Jamieson Bates <jamiesonbates@gmail.com>
2017-02-21 15:45:15 -0700

    Prepare app for deployment

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/philipsburg_tv_dev'
+  },
+  production: {
+    client: 'pg',
+    connection: 'process.env.DATABASE_URL'
   }
 };
