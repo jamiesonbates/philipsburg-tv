65ca6960318df973605169e11e8f926c285f97e9
Jamieson Bates <jamiesonbates@gmail.com>
2017-02-21 15:39:30 -0700

    Prepare app for deployment

--- server/app.js
+++ server/app.js
@@ -6,4 +6,10 @@ app.use(morgan('dev'));
 
 app.use('/api', require('./routes/api'))
 
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
 module.exports = app;
