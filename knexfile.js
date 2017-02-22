5f98e1e8f31fc58612835bf32906199121de1c60
Jamieson Bates <jamiesonbates@gmail.com>
2017-02-22 13:01:45 -0700

    Prepare the Heroku

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: 'postgres://localhost/philipsburg_tv_dev'
-  },
   production: {
     client: 'pg',
     connection: 'process.env.DATABASE_URL'
+  },
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/philipsburg_tv_dev'
   }
 };
